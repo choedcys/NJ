a9d7fff9984e98ca7e4f1698db2a82c3acee9737
choedcys <choedcys@naver.com>
2022-09-07 14:54:50 +0900

    favicon문서

--- main.js
+++ main.js
@@ -13,19 +13,25 @@ var app = http.createServer(function(request, response) {
     
     if(pathname == '/'){ // 3 root로 접속했을 경우 
         fs.readFile(`${queryData.id}`,'utf-8',function(err, description){
+            if(queryData.id === undefined){ // 1 querystring이 없이 undefined이면 홈페이지
+                // 2 함수 fs.readFile은 생략가능
+                fs.readFile(`${queryData.id}`,'utf-8',function(err, description){ // 3 지역변수 title, description 선언
+                var title = 'favicon.ico'
+                var description ='A favicon(short for favorite icon), also known as a shortcut icon, website icon,tab icon, URL icon.'
             var template = `
             <!doctype html>
             <html>
                 <head>
                     <title>WEB1 - ${title}</title> 
+                    <link rel = "icon" href = "data:,">
                     <meta charset="utf-8">
                 </head>
                 <body>
-                <h1><a href="/">WEB</a></h1>
+                <h1><a href="/">favicon이란</a></h1>
                 <ol>
-                <li><a href="/?id=13-HTML">HTML</a></li>
-                <li><a href="/?id=13-CSS">CSS</a></li>
-                <li><a href="/?id=13-JavaScript">JavaScript</a></li> 
+                <li><a href="/?id=HIS.html">History</a></li>
+                <li><a href="/?id=STAN.html">Standardization</a></li>
+                <li><a href="/?id=LEG.html">Legacy</a></li> 
                 </ol>
                 <h2>${title}</h2>
                 <p>${description}</p>
@@ -34,10 +40,39 @@ var app = http.createServer(function(request, response) {
                 response.writeHead(200); 
                 response.end(template);
             });
-        } // 4 root 이외의 경로로 접속했을 경우 
-        else {
-            response.writeHead(404); // 5 404 응답코드 : 파일 없음
-            response.end('Not found');
         }
+        else
+        {
+            fs.readFile(`${queryData.id}`,'utf-8',function(err, description){
+            var title = queryData.id 
+            var template = ` 
+            <!doctype html>
+            <html>
+                <head>
+                <title>WEB1 - ${title}</title>
+                <link rel = "icon" href = "data:,">
+                <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">favicon이란</a></h1>
+                    <ol>
+                    <li><a href="/?id=HIS.html">History</a></li>
+                    <li><a href="/?id=STAN.html">Standardization</a></li>
+                    <li><a href="/?id=LEG.html">Legacy</a></li> 
+                    </ol>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+            </html> `  ;
+            response.writeHead(200); 
+            response.end(template);
+            });
+        }
+        });
+    }
+    else{
+        response.writeHead(404);
+        response.end('Not found');
+    }
 }); 
 app.listen(3000);
